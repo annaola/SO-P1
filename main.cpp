d2cc5f771994626377d8f97915a952906973931b
iCarrrot <michal.mar3@gmail.com>
2016-11-22 13:26:10 +0100

    blah

--- main.cpp
+++ main.cpp
@@ -7,18 +7,31 @@
 #include <cstdlib>
 #include <time.h>
 #include <string>
+#include <sstream>
+
 
 using namespace std;
-class Philospher{
-	string file1,file2,id;
+string IntToString (int a){
+	stringstream ss;
+	ss << a;
+	string str = ss.str();
+	return str;
+}
 
-	void eating ( string file1, string file2,string id){
+
+
+
+class Philosopher{
+public:
+	string fork1,fork2,id;
+
+	void eating ( ){
 		fstream plik1;
 		fstream plik2;
-		plik1.open( file1, ios::in | ios::out );
+		plik1.open( fork1, ios::in | ios::out );
 		if( plik1.good() == true )
 		{
-   			plik2.open( file2, ios::in | ios::out );
+   			plik2.open( fork2, ios::in | ios::out );
    			if( plik2.good() == true ){
    				plik1<<id;
    				plik2<<id;
@@ -37,23 +50,30 @@ class Philospher{
 };
 
 int main(){
-
+	
 	string str="bla";
-	string n=0;
-	string nazwa="";
-	nazwa=str+".temp";
-	cout<<nazwa<<endl;
-	//ofstream plik( nazwa );
-	//plik<<"jsjdjdjdjd";
+	string n=IntToString(123098098);
+	string nazwa;
 
+	nazwa=str+n+".temp";
+	cout<<nazwa<<endl;
+	ofstream plik( nazwa );
+	plik<<"jsjdjdjdjd";
+
+
+	Philosopher platon;
+	platon.fork1="p1";
+	platon.fork2="p2";
+	platon.id="Platon";
+	platon.eating();
 
 	srand(time(NULL));
 	int x=0.0;
 	cout<<x;
-	while (true){
+	/*while (true){
 		x = rand()%1000000;
  		cout<<x<<str<<endl;
  		usleep(x);
- 	}
+ 	}*/
 	return 0;
 }
