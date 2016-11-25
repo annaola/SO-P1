81c49b4cb880fb94b1e5b6552f5a0f4ebe6f0a7d
annaola <biadasiewicz.anna@gmail.com>
2016-11-25 22:40:08 +0100

    update

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@
 #include <time.h>
 #include <string>
 #include <sstream>
-
+#include <semaphore.h>
 
 using namespace std;
+
+#define NPHIL 5
+
 string IntToString (int a){
 	stringstream ss;
 	ss << a;
@@ -20,10 +23,9 @@ string IntToString (int a){
 
 
 
-
-class Philosopher{
+/*class Philosopher{
 public:
-	string fork1,fork2,id;
+	string fork1, fork2, id;
 
 	void eating ( ){
 		fstream plik1;
@@ -47,11 +49,64 @@ public:
 			cout << "Dostep do pliku zostal zabroniony!" << endl;
 		}
 	}
+};*/
+
+class Philosopher
+{
+	pthread_t thread;
+	int id;
+
+public:
+	//to się chyba nazywa konstruktor :p
+	Philosopher(int i) : id{i} {
+		pthread_create(&thread, NULL, eating, NULL)
+	}
+
+	// a to jest prawdopodobnie destruktor
+	~Philosopher(){
+		pthread_join(thread, NULL)
+	}
+
+	// nie wiem, czy to powinno być tu. W sumie nie wiem nic :D
+	void think(){
+		srand(time(NULL));
+		int x = 0.0;
+		while (true){
+			x = rand()%1000000;
+	 		cout<<x<<str<<endl;
+	 		usleep(x);
+	 	}
+	}
+	
+	void eating(){
+		fstream plik1;
+		fstream plik2;
+		plik1.open( fork1, ios::in | ios::out );
+		if( plik1.good() == true )
+		{
+   			plik2.open( fork2, ios::in | ios::out );
+   			if( plik2.good() == true ){
+   				plik1<<id;
+   				plik2<<id;
+   				plik1.close();
+   				plik2.close();
+   			}
+   			else{
+   				cout << "Dostep do pliku zostal zabroniony!" << endl;
+   			}
+    //tu operacje na pliku
+		}
+		else {			
+			cout << "Dostep do pliku zostal zabroniony!" << endl;
+		}
+	}
+
+
 };
 
 int main(){
 	
-	string str="bla";
+	/*string str="bla";
 	string n=IntToString(123098098);
 	string nazwa;
 
@@ -75,6 +130,10 @@ int main(){
 		x = rand()%1000000;
  		cout<<x<<str<<endl;
  		usleep(x);
- 	}
+ 	}*/
+
+	Philosopher philosophers[NPHIL];
+	fstream forks[NPHIL];
+
 	return 0;
 }
