f67c5d2d8b759917716072acb6eeaf2847969953
annaola <biadasiewicz.anna@gmail.com>
2016-11-26 22:14:50 +0100

    Philosopher

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 #define NPHIL 5
-#define NSTOCK 7
+#define NSTOCK 3
 
 string IntToString (int a){
 	stringstream ss;
@@ -22,7 +22,6 @@ string IntToString (int a){
 	string str = ss.str();
 	return str;
 }
-
 
 
 /*truct Widelec{
@@ -43,6 +42,8 @@ class Fork
 
 public:
 	int id;
+
+	Fork() {}
 
 	Fork(int ind, int pId) : 
 		dirty{true},
@@ -78,7 +79,83 @@ public:
 
 class Philosopher
 {
+	int id;
+	int zasoby[NSTOCK]; //będziemy ją wypełniać przed "jedzeniem", żeby losowo wybierać zasoby, do których chcemy się dobrać
+
+	void chooseStocks(){
+		for (int i=1; i <= NSTOCK; i++){
+			zasoby[i] = rand() % 2;
+	};
+}
+
+public:
+	Fork forks[NSTOCK][NPHIL][NPHIL];
 	
+	Philosopher(int i, Fork fs[NSTOCK][NPHIL][NPHIL]) : id{i} {
+		for (int i = 1; i <= NSTOCK; i++){
+			for (int j = 1; j <= NPHIL; j++){
+				for (int k = 1; k <= NPHIL; k++){
+					forks[i][j][k] = fs[i][j][k];
+				}
+			}
+		}
+	}
+
+	void eat(){
+		chooseStocks();
+
+		for (int i = 1; i <= NSTOCK; i++){
+			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
+				// przeglądamy tylko połowę tablicy
+				for (int j = 1; j < id; j++){
+					forks[i][id][j].lock();
+				}
+				for (int j = id + 1; j <= NPHIL; j++){
+					forks[i][j][id].lock();
+				}
+			}
+		}
+
+		// operacja na zasobie
+
+		for (int i = 1; i <= NSTOCK; i++){
+			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
+				// przeglądamy tylko połowę tablicy
+				for (int j = 1; j < id; j++){
+					forks[i][id][j].unlock();
+				}
+				for (int j = id + 1; j <= NPHIL; j++){
+					forks[i][j][id].unlock();
+				}
+			}
+		}
+	}
+
+	void run(){
+		for (int i = 1; i <= NSTOCK; i++){
+			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
+				// przeglądamy tylko połowę tablicy
+				for (int j = 1; j < id; j++){
+					forks[i][id][j].getFork(id);
+				}
+				for (int j = id + 1; j <= NPHIL; j++){
+					forks[i][j][id].getFork(id);
+				}
+			}
+		}
+		eat();
+		for (int i = 1; i <= NSTOCK; i++){
+			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
+				// przeglądamy tylko połowę tablicy
+				for (int j = 1; j < id; j++){
+					forks[i][id][j].setDirty(true);
+				}
+				for (int j = id + 1; j <= NPHIL; j++){
+					forks[i][j][id].setDirty(true);
+				}
+			}
+		}
+	}
 };
 
 /*class Philosopher
