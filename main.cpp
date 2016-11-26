d1f437cd148f5a46d0743b15c852fc1d2b9ef294
iCarrrot <michal.mar3@gmail.com>
2016-11-26 13:09:56 +0100

    blah

--- main.cpp
+++ main.cpp
@@ -25,15 +25,14 @@ string IntToString (int a){
 class Philosopher
 {
 	pthread_t thread;
-	bool forks[NFORK];
-
+	//bool forks[NFORK];
+/*
 	void fillForks(){
 		for (int i; i <= NFORK; i++){
 			forks[i] = rand() % 2;
 		};
 	}
-
-	// nie wiem, czy to powinno być tu. W sumie nie wiem nic :D
+*/
 	void think(){
 		int x = 0.0;
 		while (true){
@@ -44,40 +43,22 @@ class Philosopher
 	
 	// to trzeba dopracować
 	void eating(){
-		/*fstream plik1;
-		fstream plik2;
-		plik1.open( fork1, ios::in | ios::out );
-		if( plik1.good() == true )
-		{
-   			plik2.open( fork2, ios::in | ios::out );
-   			if( plik2.good() == true ){
-   				plik1<<id;
-   				plik2<<id;
-   				plik1.close();
-   				plik2.close();
-   			}
-   			else{
-   				cout << "Dostep do pliku zostal zabroniony!" << endl;
-   			}
-    //tu operacje na pliku
-		}
-		else {			
-			cout << "Dostep do pliku zostal zabroniony!" << endl;
-		}*/
+
 	}
 
 	// a to w ogóle napisać
 	static void *run(void *arg){
-		// tu bym umieściła coś podonego do Rysunku 6.13. ze Stallingsa
-		// połączenie eating i think 
+
 		return NULL;
 	}
 
 public:
-	long id;
+	int m;
+	int id;
+	int pliki[m];
 
-	//to się chyba nazywa konstruktor :p
-	Philosopher() {}
+
+/*	Philosopher() {}
 
 	Philosopher(long i) : id{i}{
 		//thread t1(&thread_1);
@@ -87,6 +68,13 @@ public:
 	// a to jest prawdopodobnie destruktor
 	~Philosopher(){
 		//pthread_join(thread, NULL);
+	}
+	*/
+
+	Philosopher(int IdOfHim, int LiczbaPlikow): m(LiczbaPlikow),id(IdOfHim){
+		for (int i=0;i<m;i++){
+			plik[i]=rand()%2;
+		}
 	}
 };
 
