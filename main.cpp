d4ffcca8264bf1e07381857631260b47b14dd48c
iCarrrot <michal.mar3@gmail.com>
2016-11-27 22:14:57 +0100

    działa

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 
 #define NPHIL 5
-#define NSTOCK 3
+#define NSTOCK 6
 #define N 10
 
 string IntToString (int a){
@@ -93,7 +93,7 @@ public:
 	void chooseStocks(){
 		for (int i=0; i < NSTOCK; i++){
 			zasoby[i] = rand() % 2;
-			//cout<<id<<" generowanie zasobów  "<<zasoby[i]<<endl;
+			
 		}
 
 	}
@@ -108,7 +108,7 @@ public:
 	void wyswietl_zasob(){
 		for (int j = 0; j < NSTOCK; ++j)
 		{
-			cout<<id<<" wyswietl_zasob(): "<<zasoby[j]<<endl;
+			
 		}
 
 	}
@@ -118,17 +118,17 @@ public:
 		for (int i = 0; i < NSTOCK; i++){
 			
 			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
-				cout<< id << " próbuje sie dostac do zasobu nr "<< i<<endl;
+				
 				// przeglądamy tylko połowę tablicy
 				for (int j = 0; j < id; j++){
-					cout << id << "chce zablokowac widelec od " << j<< endl;
+					
 					forks[i][id][j]->lock();
-					cout << "widelec zablokowany " << id<< j<< endl;
+					
 				}
 				for (int j = id + 1; j < NPHIL; j++){
-					cout << id << "chce zablokowac widelec od " << j << endl;;
+					
 					forks[i][j][id]->lock();
-					cout << "widelec zablokowany " << id<< j << endl;
+					
 				}
 			}
 		}
@@ -139,13 +139,13 @@ public:
 			if (zasoby[i]==1){
 				string x;
 				x=IntToString(i)+".temp";
-				cout<<"dostal sie "<<endl;
+				
 				plik[i].open( x,  ios::out |ios::app);
 				if( plik[i].good() == true )
 				{
 					plik[i]<<id<<endl;
 				    plik[i].close();
-				} else cout << "Dostep do pliku zostal zabroniony!" << endl;
+				} else cout <<"brak dostępu";
 			}
 		}
 
@@ -153,14 +153,14 @@ public:
 			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
 				// przeglądamy tylko połowę tablicy
 				for (int j = 0; j < id; j++){
-					cout << id << "chce odblokować widelec od " << j <<endl;
+					
 					forks[i][id][j]->unlock();
-					cout << "widelec odblokowany " << id<<j<< endl;
+					
 				}
 				for (int j = id + 1; j < NPHIL; j++){
-					cout << id << "chce odblokować widelec od " << j <<endl;
+					
 					forks[i][j][id]->unlock();
-					cout << "widelec odblokowany " << id<< j << endl;
+					
 				}
 			}
 		}
@@ -168,22 +168,22 @@ public:
 
 	void run(){
 		think();
-		cout << "filozof " << id << " jest głodny" << endl;
+		
 		for (int i = 0; i < NSTOCK; i++){
 
-			cout << "bla" <<id << endl;
+			
 			if (zasoby[i] == 1){ //jeśli filozof chce dostępu do danego zasobu
 				// przeglądamy tylko połowę tablicy
-				cout << "blah" << id<< endl;
+				
 				for (int j = 0; j < id; j++){
-					cout << id << "chce zabrać widelec od " << j << endl;
+					
 					forks[i][id][j]->getFork(id);
-					cout << "widelec zabrany " << id<< j << endl;
+					
 				}
 				for (int j = id + 1; j < NPHIL; j++){
-					cout << id << "chce zabrać widelec od " << j << endl;
+					
 					forks[i][j][id]->getFork(id);
-					cout << "widelec zabrany " << id<< j << endl;
+					
 				}
 			}
 		}
@@ -193,11 +193,11 @@ public:
 				// przeglądamy tylko połowę tablicy
 				for (int j = 0; j < id; j++){
 					forks[i][id][j]->setDirty(true);
-					cout << "widelec brudny " << id << j << endl;
+					
 				}
 				for (int j = id + 1; j < NPHIL; j++){
 					forks[i][j][id]->setDirty(true);
-					cout << "widelec brudny "  << id<< j<< endl;
+					
 				}
 			}
 		}
@@ -222,10 +222,7 @@ void eatForYourLive (Philosopher* platon){
 	for(int i = 0; i < N; i++){
 		platon->run();
 	}
-	/*for (int i = 0; i < NSTOCK; ++i)
-	{
-		//cout<<"może tu: "<<platon->zasoby[i];
-	}*/
+
 }
 
 
@@ -236,63 +233,50 @@ int main(){
 	Fork forks[NSTOCK][NPHIL][NPHIL];
 	Philosopher philosophers[NPHIL];
 
-	cout << "widelce i filozofowie stworzeni" << endl;
 	//tworzymy filozofów:
 
 	for (int i=0; i < NPHIL; i++)
 	{
 		Philosopher p = Philosopher(i,forks);
 		p.chooseStocks();
-		for (int j = 0; j < NSTOCK; j++)
-		{
-			cout << "filozof " << i << "chce zasobów" << p.zasoby[j] << endl;
-		}
 		
 		philosophers[i] = p;
 	}
-
-	cout << "filozofowie wiedzą, których widelców potrzebują" << endl;
 
 	//Tworzymy widelce
 
 	for (int i = 0; i < NSTOCK; i++){
 		for (int j = 0; j < NPHIL; j++){
 			for (int k = 0; k < NPHIL; k++){
-				//cout<<"widelce  "<< philosophers[j].zasoby[i]<<"  "<<philosophers[k].zasoby[i]<<endl;
-				//if (philosophers[j].zasoby[i]==philosophers[k].zasoby[i]&&philosophers[j].zasoby[i]==1){
+				
 
 					int lower=(j<k)?j:k;
 					
 					Fork t = Fork(i*100+j*10+k,lower,i);
 					forks[i][j][k] = t;
 				
-				//cout<<i<<" "<<j<<" "<<k<<endl;
 				
 			}
 		}
 	}
 
 
-	cout << "przypisanie widelców" << endl;
+	
 
 	thread philosophersThreads[NPHIL]; //tablica wątków
 
-	cout << "tablica wątków" << endl;
+	
 
 	for (int i=0; i<NPHIL; i++){
-		cout<<"odpalamy jedzenie filozofa nr: "<<i<<endl;
+		
 		Philosopher *p1;
 		p1= &(philosophers[i]);
-		/*
-		for (int j = 0; j < NSTOCK; ++j)
-		{
-			cout<<"przypisywanie: "<<p1->zasoby[j]<<" "<<" "<<philosophers[i].zasoby[j]<<endl;
-		}*/
+		
 		
 		philosophersThreads[i] = thread(&eatForYourLive, p1);	
 	}
 
-	cout << "wątki uruchomione" << endl;
+
 
     for (int i=0; i<NPHIL; i++){
         philosophersThreads[i].join();
