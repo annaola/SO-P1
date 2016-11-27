3bcca67dace5a01dd6496a7ec4d4d313dba2d9af
iCarrrot <michal.mar3@gmail.com>
2016-11-27 10:08:45 +0100

    blah

--- main.cpp
+++ main.cpp
@@ -23,65 +23,31 @@ string IntToString (int a){
 }
 
 struct Widelec{
-int id[1][1]; //pierwsze pole to pierwszy filozof, drugie pole to drugi filozof, pomiędzy którymi jest widelec
-int idFilozofa; //id aktualnie trzymającego świdelec filozofa
-bool stan; //0-czysty,1-brudny
+	int id[1][1]; //pierwsze pole to pierwszy filozof, drugie pole to drugi filozof, pomiędzy którymi jest widelec
+	int idFilozofa; //id aktualnie trzymającego świdelec filozofa
+	semafor stan; //0-czysty,1-brudny
 };
-
-
-
-
-
 
 
 class Philosopher
 {
-	pthread_t thread;
-	//bool forks[NFORK];
-/*
-	void fillForks(){
-		for (int i; i <= NFORK; i++){
-			forks[i] = rand() % 2;
-		};
-	}
-*/
+	
 	void think(){
-		int x = 0.0;
+		int x = 0;
 		while (true){
 			x = rand()%1000000;
 	 		usleep(x);
 	 	}
 	}
 	
-	// to trzeba dopracować
-	void eating(){
-
-	}
-
-	// a to w ogóle napisać
-	static void *run(void *arg){
-
-		return NULL;
-	}
-
+	
 public:
 	int m;//liczba plików
 	int id;
 	int pliki[m];
-
-
-/*	Philosopher() {}
-
-	Philosopher(long i) : id{i}{
-		//thread t1(&thread_1);
-		pthread_create(&thread, NULL, run, (void*) id); // już nie krzyczy
-		fillForks();// tu funkcja generująca losowe boole w tablicy forks
+	void eat(Widelec* tab[]) {
+		for (int i=0; i<)
 	}
-	// a to jest prawdopodobnie destruktor
-	~Philosopher(){
-		//pthread_join(thread, NULL);
-	}
-	*/
 
 	Philosopher(int IdOfHim, int LiczbaPlikow): m(LiczbaPlikow),id(IdOfHim){
 		for (int i=0;i<m;i++){
